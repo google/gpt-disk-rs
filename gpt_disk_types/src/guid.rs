da4077f0ea13b76cdeb0e39b2af3394d21f0518d
Nicholas Bishop <nicholasbishop@google.com>
2022-06-10 14:38:50 -0400

    Improve examples in Guid docstring

--- gpt_disk_types/src/guid.rs
+++ gpt_disk_types/src/guid.rs
@@ -80,8 +80,27 @@ const fn parse_byte_from_ascii_str_at(
 ///
 /// # Examples
 ///
+/// Construct a GUID at compile time with the `guid!` macro:
+///
+/// ```
+/// use gpt_disk_types::guid;
+///
+/// let guid = guid!("01234567-89ab-cdef-0123-456789abcdef");
+/// ```
+///
+/// Parse a GUID at runtime from a string:
+///
+/// ```
+/// use gpt_disk_types::Guid;
+///
+/// let guid: Guid = "01234567-89ab-cdef-0123-456789abcdef".parse().unwrap();
+/// ```
+///
+/// Construct a GUID from its components:
+///
 /// ```
 /// use gpt_disk_types::{Guid, U16Le, U32Le};
+///
 /// let guid = Guid::new(
 ///     U32Le::from_u32(0x01234567),
 ///     U16Le::from_u16(0x89ab),
@@ -90,14 +109,15 @@ const fn parse_byte_from_ascii_str_at(
 ///     0x23,
 ///     [0x45, 0x67, 0x89, 0xab, 0xcd, 0xef],
 /// );
-/// assert_eq!(guid.to_string(), "01234567-89ab-cdef-0123-456789abcdef");
+/// ```
 ///
-/// assert_eq!(
-///     "01234567-89ab-cdef-0123-456789abcdef"
-///         .parse::<Guid>()
-///         .unwrap(),
-///     guid
-/// );
+/// Convert to a string:
+///
+/// ```
+/// use gpt_disk_types::guid;
+///
+/// let guid = guid!("01234567-89ab-cdef-0123-456789abcdef");
+/// assert_eq!(guid.to_string(), "01234567-89ab-cdef-0123-456789abcdef");
 /// ```
 #[derive(
     Clone, Copy, Debug, Eq, PartialEq, Hash, Ord, PartialOrd, Pod, Zeroable,
