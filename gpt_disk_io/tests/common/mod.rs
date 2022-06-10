c5e2bfb777387fa061345f07fd88bd77f2cbfd94
Nicholas Bishop <nicholasbishop@google.com>
2022-06-10 11:17:35 -0400

    Update tests to use the new guid! macro

--- gpt_disk_io/tests/common/mod.rs
+++ gpt_disk_io/tests/common/mod.rs
@@ -14,7 +14,9 @@
 
 use core::fmt::{Debug, Display};
 use core::hash::Hash;
-use gpt_disk_types::{Crc32, GptHeader, GptPartitionEntry, LbaLe, U32Le};
+use gpt_disk_types::{
+    guid, Crc32, GptHeader, GptPartitionEntry, GptPartitionType, LbaLe, U32Le,
+};
 use std::collections::hash_map::DefaultHasher;
 
 #[allow(dead_code)]
@@ -60,7 +62,7 @@ pub fn create_primary_header() -> GptHeader {
         alternate_lba: LbaLe::from_u64(8191),
         first_usable_lba: LbaLe::from_u64(34),
         last_usable_lba: LbaLe::from_u64(8158),
-        disk_guid: "57a7feb6-8cd5-4922-b7bd-c78b0914e870".parse().unwrap(),
+        disk_guid: guid!("57a7feb6-8cd5-4922-b7bd-c78b0914e870"),
         partition_entry_lba: LbaLe::from_u64(2),
         number_of_partition_entries: U32Le::from_u32(128),
         partition_entry_array_crc32: Crc32(U32Le::from_u32(0x9206adff)),
@@ -82,12 +84,10 @@ pub fn create_secondary_header() -> GptHeader {
 #[allow(dead_code)]
 pub fn create_partition_entry() -> GptPartitionEntry {
     GptPartitionEntry {
-        partition_type_guid: "ccf0994f-f7e0-4e26-a011-843e38aa2eac"
-            .parse()
-            .unwrap(),
-        unique_partition_guid: "37c75ffd-8932-467a-9c56-8cf1f0456b12"
-            .parse()
-            .unwrap(),
+        partition_type_guid: GptPartitionType(guid!(
+            "ccf0994f-f7e0-4e26-a011-843e38aa2eac"
+        )),
+        unique_partition_guid: guid!("37c75ffd-8932-467a-9c56-8cf1f0456b12"),
         starting_lba: LbaLe::from_u64(2048),
         ending_lba: LbaLe::from_u64(4096),
         attributes: Default::default(),
