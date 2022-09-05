a2ec51f2bcad095e87032f3174f3a14da052c27e
Nicholas Bishop <nbishop@nbishop.net>
2022-09-04 23:36:09 -0400

    uguid: Add AlignedGuid tests

--- uguid/tests/test_serde.rs
+++ uguid/tests/test_serde.rs
@@ -9,11 +9,30 @@
 #![cfg(feature = "serde")]
 
 use serde_test::Token;
-use uguid::{guid, Guid};
+use uguid::{aligned_guid, guid, AlignedGuid, Guid};
 
 #[test]
-fn test_serde() {
+fn test_serde_unaligned() {
     let guid = guid!("01234567-89ab-cdef-0123-456789abcdef");
+
+    serde_test::assert_tokens(
+        &guid,
+        &[Token::Str("01234567-89ab-cdef-0123-456789abcdef")],
+    );
+
+    serde_test::assert_de_tokens_error::<Guid>(
+        &[Token::Str("1234")],
+        "GUID string has wrong length (expected 36 bytes)",
+    );
+
+    serde_test::assert_de_tokens_error::<Guid>(
+        &[Token::U64(1234)],
+        "invalid type: integer `1234`, expected a string in the format \"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx\"");
+}
+
+#[test]
+fn test_serde_aligned() {
+    let guid = aligned_guid!("01234567-89ab-cdef-0123-456789abcdef");
 
     serde_test::assert_tokens(
         &guid,
