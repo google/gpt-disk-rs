dd3e9ce85818b35f5af9334019593c625efcd466
Nicholas Bishop <nbishop@nbishop.net>
2022-09-05 17:44:51 -0400

    uguid: Add initial compilation UI test

---
+++ compilation_test/tests/compilation_test.rs
@@ -0,0 +1,13 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[test]
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/*.rs");
+}
