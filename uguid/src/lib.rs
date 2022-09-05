965ba246dfeb78b69a725cb4019a56cdd13faa33
Nicholas Bishop <nbishop@nbishop.net>
2022-09-04 21:52:37 -0400

    uguid: Add util module

--- uguid/src/lib.rs
+++ uguid/src/lib.rs
@@ -86,6 +86,8 @@
 #![warn(clippy::as_conversions)]
 #![allow(clippy::missing_errors_doc)]
 
+mod util;
+
 mod guid_parse;
 pub use guid_parse::GuidFromStrError;
 
@@ -97,22 +99,7 @@ use bytemuck::{Pod, Zeroable};
 
 use core::fmt::{self, Display, Formatter};
 use core::str::{self, FromStr};
-
-const fn byte_to_ascii_hex_lower(byte: u8) -> (u8, u8) {
-    let mut l = byte & 0xf;
-    let mut h = byte >> 4;
-    if l <= 9 {
-        l += b'0';
-    } else {
-        l += b'a' - 10;
-    }
-    if h <= 9 {
-        h += b'0';
-    } else {
-        h += b'a' - 10;
-    }
-    (h, l)
-}
+use util::byte_to_ascii_hex_lower;
 
 /// Globally-unique identifier.
 ///
@@ -300,15 +287,4 @@ macro_rules! guid {
             Err(_) => panic!("invalid GUID string"),
         }
     };
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_to_ascii() {
-        assert_eq!(byte_to_ascii_hex_lower(0x1f), (b'1', b'f'));
-        assert_eq!(byte_to_ascii_hex_lower(0xf1), (b'f', b'1'));
-    }
 }
