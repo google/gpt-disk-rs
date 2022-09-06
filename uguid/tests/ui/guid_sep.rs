2a7d85b5bdba5927e8b72e75db12af59199c0408
Nicholas Bishop <nbishop@nbishop.net>
2022-09-06 19:28:51 -0400

    uguid: Add more macro tests

---
+++ uguid/tests/ui/guid_sep.rs
@@ -0,0 +1,13 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use uguid::guid;
+
+pub fn f() {
+    let _g = guid!("01234567089ab-cdef-0123-456789abcdef");
+}
