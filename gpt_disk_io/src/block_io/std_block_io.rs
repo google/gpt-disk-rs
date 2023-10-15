0027ce84fd4e8bb237d535bf4f411a4a4d080cb5
Nicholas Bishop <nbishop@nbishop.net>
2023-10-14 21:48:48 -0400

    gpt_disk_io: Add File impls for BlockIoAdapter

--- gpt_disk_io/src/block_io/std_block_io.rs
+++ gpt_disk_io/src/block_io/std_block_io.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use crate::BlockIo;
+use crate::{BlockIo, BlockIoAdapter};
 use gpt_disk_types::{BlockSize, Lba};
 use std::fs::File;
 use std::io::{self, Read, Seek, SeekFrom, Write};
@@ -143,3 +143,70 @@ pub trait ReadWriteSeek: Read + Write + Seek {
 impl ReadWriteSeek for File {}
 impl ReadWriteSeek for &File {}
 impl<T> ReadWriteSeek for &mut T where T: Read + Write + Seek {}
+
+impl<T> BlockIo for BlockIoAdapter<T>
+where
+    T: ReadWriteSeek,
+{
+    type Error = io::Error;
+
+    fn block_size(&self) -> BlockSize {
+        self.block_size
+    }
+
+    fn num_blocks(&mut self) -> Result<u64, Self::Error> {
+        self.storage.num_blocks(self.block_size)
+    }
+
+    fn read_blocks(
+        &mut self,
+        start_lba: Lba,
+        dst: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.storage.read_blocks(self.block_size, start_lba, dst)
+    }
+
+    fn write_blocks(
+        &mut self,
+        start_lba: Lba,
+        src: &[u8],
+    ) -> Result<(), Self::Error> {
+        self.storage.write_blocks(self.block_size, start_lba, src)
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        self.storage.flush()
+    }
+}
+
+impl BlockIo for BlockIoAdapter<&mut dyn ReadWriteSeek> {
+    type Error = io::Error;
+
+    fn block_size(&self) -> BlockSize {
+        self.block_size
+    }
+
+    fn num_blocks(&mut self) -> Result<u64, Self::Error> {
+        self.storage.num_blocks(self.block_size)
+    }
+
+    fn read_blocks(
+        &mut self,
+        start_lba: Lba,
+        dst: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.storage.read_blocks(self.block_size, start_lba, dst)
+    }
+
+    fn write_blocks(
+        &mut self,
+        start_lba: Lba,
+        src: &[u8],
+    ) -> Result<(), Self::Error> {
+        self.storage.write_blocks(self.block_size, start_lba, src)
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        self.storage.flush()
+    }
+}
