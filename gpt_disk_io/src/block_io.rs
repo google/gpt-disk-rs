6d40e77b5f3d589b39ed1c6b1b4c94ee3d2028ce
Nicholas Bishop <nbishop@nbishop.net>
2023-10-15 15:29:02 -0400

    gpt_disk_io: Expand BlockIoAdapter doc

--- gpt_disk_io/src/block_io.rs
+++ gpt_disk_io/src/block_io.rs
@@ -15,6 +15,8 @@ use core::fmt::{Debug, Display};
 use gpt_disk_types::{BlockSize, Lba};
 
 /// Trait for reading from and writing to a block device.
+///
+/// See also [`BlockIoAdapter`].
 pub trait BlockIo {
     /// IO error type.
     type Error: Debug + Display + Send + Sync + 'static;
@@ -68,6 +70,88 @@ pub trait BlockIo {
 /// Adapter for types that can act as storage, but don't have a block
 /// size. This is used to provide `BlockIo` impls for byte slices,
 /// files, and various other types.
+///
+/// Note that `BlockIoAdapter<T>` can be constructed for any sized type
+/// `T`, but not all types provide a `BlockIo` impl for
+/// `BlockIoAdapter<T>`.
+///
+/// # For byte slices
+///
+/// ```
+/// use gpt_disk_io::gpt_disk_types::{BlockSize, Lba};
+/// use gpt_disk_io::{BlockIo, BlockIoAdapter, SliceBlockIoError};
+///
+/// let mut one_block = [0; 512];
+///
+/// // Construct `BlockIoAdapter` for an immutable byte slice:
+/// let data: &[u8] = &[0; 1024];
+/// let mut bio = BlockIoAdapter::new(data, BlockSize::BS_512);
+/// // Demonstrate that reading succeeds:
+/// assert!(bio.read_blocks(Lba(0), &mut one_block).is_ok());
+/// // But writing fails since the storage is immutable:
+/// assert!(bio.write_blocks(Lba(0), &one_block).is_err());
+///
+/// // Construct `BlockIoAdapter` for a mutable byte slice:
+/// let data: &mut [u8] = &mut [0; 512];
+/// let mut bio = BlockIoAdapter::new(data, BlockSize::BS_512);
+/// // Now both reading and writing succeed:
+/// assert!(bio.read_blocks(Lba(0), &mut one_block).is_ok());
+/// assert!(bio.write_blocks(Lba(0), &one_block).is_ok());
+/// ```
+///
+/// # With the `alloc` feature
+///
+/// Construct a `BlockIoAdapter` that owns a `Vec<u8>`:
+///
+/// ```
+/// use gpt_disk_io::gpt_disk_types::BlockSize;
+/// use gpt_disk_io::{BlockIo, BlockIoAdapter, SliceBlockIoError};
+///
+/// #[cfg(feature = "alloc")]
+/// fn example_alloc() -> Result<(), SliceBlockIoError> {
+///     let data: Vec<u8> = vec![0; 512];
+///     let mut bio = BlockIoAdapter::new(data, BlockSize::BS_512);
+///     assert_eq!(bio.num_blocks()?, 1);
+///
+///     Ok(())
+/// }
+/// ```
+///
+/// # With the `std` feature
+///
+/// Construct `BlockIoAdapter` from various file-like types:
+///
+/// ```
+/// use gpt_disk_io::gpt_disk_types::BlockSize;
+/// use gpt_disk_io::{BlockIo, BlockIoAdapter};
+/// use std::fs::{self, File};
+/// use std::io::{self, Cursor};
+/// use std::path::Path;
+///
+/// #[cfg(feature = "std")]
+/// fn example_std(path: &Path) -> Result<(), io::Error> {
+///     // Construct a `BlockIoAdapter` that takes ownership of a file.
+///     // This also works for any type that implements the `ReadWriteSeek` trait.
+///     let file = File::open(path)?;
+///     let mut bio = BlockIoAdapter::new(file, BlockSize::BS_512);
+///     assert_eq!(bio.num_blocks()?, 1);
+///
+///     // Construct a `BlockIoAdapter` that borrows a file.
+///     let file = File::open(path)?;
+///     let mut bio = BlockIoAdapter::new(&file, BlockSize::BS_512);
+///     assert_eq!(bio.num_blocks()?, 1);
+///
+///     // Construct a `BlockIoAdapter` from another type that
+///     // implements `Read + Write + Seek`, but does not directly implement
+///     // `ReadWriteSeek`. Due to trait constraints, this requires an `&mut`
+///     // borrow; transferring ownership of `cursor` would fail to compile.
+///     let mut cursor = Cursor::new(vec![0; 512]);
+///     let mut bio = BlockIoAdapter::new(&mut cursor, BlockSize::BS_512);
+///     assert_eq!(bio.num_blocks()?, 1);
+///
+///     Ok(())
+/// }
+/// ```
 #[allow(clippy::module_name_repetitions)]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct BlockIoAdapter<T> {
