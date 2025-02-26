bd857da5d19d51c6900c4d4b72947ce8a8f986d2
Nicholas Bishop <nicholasbishop@google.com>
2025-02-26 17:31:48 -0500

    gpt_disk_types: Fix indentation of list
    This was caught by clippy 1.86 (currently on the beta channel).

--- gpt_disk_types/src/lib.rs
+++ gpt_disk_types/src/lib.rs
@@ -42,8 +42,8 @@
 //! # Features
 //!
 //! * `bytemuck`: Implements bytemuck's `Pod` and `Zeroable` traits for
-//!    many of the types in this crate. Also enables some methods that
-//!    rely on byte access.
+//!   many of the types in this crate. Also enables some methods that
+//!   rely on byte access.
 //! * `std`: Provides `std::error::Error` implementations for all of the
 //!   error types. Off by default.
 //!
